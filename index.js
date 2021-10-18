04a61d9ee634b4719242581da2772822fdde5a86
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-10-18 17:25:48 +0300

    done server with db postgreSQL

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const userRouter = require('./routes/user.router');
+const postRouter = require('./routes/post.router');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', userRouter);
+app.use('/api', postRouter);
 
 
 const start = async () => {
